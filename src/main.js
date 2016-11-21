c8e97a70a4fdbcbed0a4c1bea2b01a112d69eb48
sindu12jun <imgonnalovetheworld@gmail.com>
2016-11-22 01:15:22 +0800

    solve too long problem

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import OriginTable from './OriginTable';
 
-const table = window.document.querySelector('#table');
+const table1 = window.document.querySelector('#table1');
+const table2 = window.document.querySelector('#table2');
 
 /* eslint-disable no-new */
-new OriginTable(table);
+new OriginTable(table1);
+new OriginTable(table2);
