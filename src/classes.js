7799c34d2e0994982314dd4cb74df6674a70f20c
lijun30 <iamdavidli@outlook.com>
2019-01-22 23:19:57 +0800

    init basic

---
+++ src/classes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lijun on 2016/12/16.
+ */
+export default {
+  originTable: 'sindu_origin_table',
+  fakeTable: 'sindu_dragger',
+  dragging: 'sindu_dragging',
+  static: 'sindu_static',
+  handle: 'sindu_handle',
+};
+
