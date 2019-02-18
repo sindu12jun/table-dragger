a47a00619ef319b13a6ded552e72fa5184587134
lijun30 <iamdavidli@outlook.com>
2019-02-18 23:39:56 +0800

    write with rxjs and ramda

--- src/dragger.js
+++ src/dragger.js
@@ -5,8 +5,6 @@ export default function tableDragger(table, userOptions) {
   const options = Object.assign({}, defaultOptions, userOptions)
   const {dragHandler, mode: optionMode} = options
   const handlers = getHandlers(table, options, dragHandler)
-  const down$ = Rx.fromEvent(handlers, 'mousedown')
-  const realMode = R.pipe(getMoveDirection, R.partial(getRealMode, [optionMode]))
   {
     const errorMsg = checkTable(table, options)
     if (errorMsg) throw new Error(errorMsg)
