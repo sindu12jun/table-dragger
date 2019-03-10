27f3014260b6f7d0e9e8cc4c1302fd36203e5f66
lijun30 <iamdavidli@outlook.com>
2019-03-10 22:31:34 +0800

    add helpers

--- src/helper.js
+++ src/helper.js
@@ -4,3 +4,54 @@ export function setCSSes(el, csses) {
   });
   return el;
 }
+
+export function addClass(ele, className) {
+  ele.classList.add(className)
+  return ele
+}
+
+export function removeClass(ele, className) {
+  ele.classList.remove(className)
+  return ele
+}
+
+export function removeAttrs(ele, attrs) {
+  R.forEach(ele.removeAttribute.bind(ele))(attrs)
+  return ele
+}
+
+export function isLeftButton(e) {
+  if ('touches' in e) {
+    return e.touches.length === 1;
+  }
+  if ('buttons' in e) {
+    return e.buttons === 1;
+  }
+  if ('button' in e) {
+    return e.button === 0;
+  }
+  return false;
+}
+
+export function checkIsTable(ele) {
+  return ele
+    &&
+    typeof ele === 'object'
+    &&
+    'nodeType' in ele
+    &&
+    ele.nodeType === 1
+    &&
+    ele.cloneNode
+    &&
+    ele.nodeName === 'TABLE';
+}
+
+export function emptyContent(ele) {
+  ele.innerHTML = ''
+  return ele
+}
+
+
+
+
