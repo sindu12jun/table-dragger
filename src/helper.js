69372734d7896a294215d8b735a590770e54c866
lijun30 <iamdavidli@outlook.com>
2019-04-02 23:11:03 +0800

    update helpers

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
+import * as R from "ramda";
+
 export function setCSSes(el, csses) {
   Object.keys(csses).forEach((k) => {
     el.style[k] = csses[k];
@@ -7,16 +9,6 @@ export function setCSSes(el, csses) {
 
 export function addClass(ele, className) {
   ele.classList.add(className)
-  return ele
-}
-
-export function removeClass(ele, className) {
-  ele.classList.remove(className)
-  return ele
-}
-
-export function removeAttrs(ele, attrs) {
-  R.forEach(ele.removeAttribute.bind(ele))(attrs)
   return ele
 }
 
@@ -58,7 +50,12 @@ export function isNodeCol(node) {
 
 export function appendDOMChild(parentNode, childNode) {
   // !!SIDE EFFECTS!!
+  parentNode.appendChild(childNode);
   return parentNode;
+}
+
+export function rect(node) {
+  return node.getBoundingClientRect()
 }
 
 export function setStyle(node, prop, value) {
@@ -79,14 +76,38 @@ export function addPx(str) {
   return str + 'px'
 }
 
+// insert target before origin
+export function insertBeforeSibling(toEle, fromEle) {
+  if (!fromEle) {
+    return;
+  }
+  toEle.parentNode.insertBefore(fromEle, toEle);
+  return fromEle
+}
+
+// insert target after origin
 export function appendSibling(toEle, fromEle) {
   if (!fromEle) {
     return;
   }
   // if row length is different
-  toEle.insertBefore(fromEle, toEle ? toEle.nextElementSibling : null);
+  toEle.parentNode.insertBefore(fromEle, toEle ? toEle.nextElementSibling : null);
 };
 
+export function getMouseDownEvent() {
+  let event;
+  if (document.createEvent) {
+    event = document.createEvent("MouseEvent");
+    event.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  } else {
+    event = new MouseEvent('mousedown', {
+      'view': window,
+      'bubbles': true,
+      'cancelable': true
+    });
+  }
+  return event;
+};
 
 
 
