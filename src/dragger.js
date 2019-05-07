21d5871a464e1d81a86ac9353f8e986dd7792b3b
lijun30 <iamdavidli@outlook.com>
2019-05-07 18:23:43 +0800

    sort

--- src/dragger.js
+++ src/dragger.js
@@ -37,6 +37,105 @@ const defaultOptions = {
   animation: 300,
 };
 
+export function getTheFirstColumn(table) {
+  return R.map(R.curry(getCellByIndexInRow)(0))(table.rows)
+}
+
+export function getTheFistRow(table) {
+  return table.rows[0].children
+}
+
+export function checkTable(table, options) {
+  let errorMsg
+  if (!checkIsTable(table)) {
+    errorMsg = `table-dragger: el must be TABLE HTMLElement, not ${{}.toString.call(table)}`;
+  }
+  if (options.mode === 'free' && !options.dragHandler) {
+    errorMsg = 'table-dragger: please specify dragHandler in free mode';
+  }
+  if (!table.rows.length) {
+    errorMsg = 'table-dragger: there are no cells in the table';
+  }
+  return errorMsg
+}
+
+function isMousedownValid(mousedownEvent) {
+  const ignore = !isLeftButton(mousedownEvent) || mousedownEvent.metaKey || mousedownEvent.ctrlKey;
+  return !ignore
+}
+
+export function getRealMode(optionMode, moveDirection) {
+  if (!moveDirection) {
+    return
+  }
+  return optionMode === 'free' ? moveDirection : optionMode === 'row' ? rowType : columnType
+}
+
+export function getTableLength(table, mode) {
+  const getRowLength = R.compose(
+    R.partialRight(prop, ['length']),
+    R.partialRight(prop, ['children'])
+  )
+  return mode === rowType ?
+    table.rows.length :
+    R.compose(R.apply(Math.max), R.map(getRowLength), ArrayFrom)(table.rows)
+}
+
+export function getFakeTables(table, mode) {
+  const tableLength = getTableLength(table, mode)
+  return R.map(R.partial(getFakeTableByIndex, [table, mode]))(createArrByNumber(tableLength))
+}
+
+export function getTargetIndexInTable(target, mode) {
+  while (target.nodeName !== 'TD' && target.nodeName !== 'TH') {
+    target = target.parentElement;
+  }
+  return mode === rowType ? target.parentElement.rowIndex : target.cellIndex
+}
+
+
+export function getMoveDirection(downEvent, moveEvent) {
+  const gapX = Math.abs(moveEvent.clientX - downEvent.clientX)
+  const gapY = Math.abs(moveEvent.clientY - downEvent.clientY)
+  if (gapX === gapY) return null
+  return gapX > gapY ? columnType : rowType
+}
+
+/**
+ * Put fromEle after toEle
+ * @param fromEle
+ * @param toEle
+ * @param isForward if true, down or right
+ */
+export function sortElements(fromEle, toEle, isForward) {
+  if (isForward) {
+    appendSibling(toEle, fromEle);
+  } else {
+    insertBeforeSibling(toEle, fromEle);
+  }
+}
+
+export function exchangeColumns(table, from, to) {
+  if (from === to) {
+    return;
+  }
+  Array.from(table.rows).forEach((row) => {
+    sortElements(row.children[from], row.children[to], from < to)
+  });
+
+  const cols = table.querySelectorAll('col');
+  if (cols.length) {
+    sortElements(cols[from], cols[to], from < to);
+  }
+}
+
+export function exchangeRows(table, from, to) {
+  if (from === to) {
+    return;
+  }
+  const list = Array.from(table.rows);
+  sortElements(list[from], list[to], from < to)
+}
 
 export function onDrag(dragger, table, mode,) {
   dragger.dragging = true
@@ -64,28 +163,27 @@ export function onShadow(targetIndex, fakeTable, mode, table, dragger, draggingI
   const to = Array.from(fakeTable.children).indexOf(draggingItem);
   dragger.emit('shadowMove', from, to, table, modeString(mode));
 }
-function removeAllCols(table) {
-  const cols = [...ArrayFrom(table.querySelectorAll('col')), ...ArrayFrom(table.querySelectorAll('colgroup'))]
-  R.forEach(removeDom)(cols)
-  return table
 
-
-export function exchangeRows(table, from, to) {
-  if (from === to) {
-    return;
-  }
-  const list = Array.from(table.rows);
-  sortElements(list[from], list[to], from < to)
+export function onOut(mode, table, dragger) {
+  dragger.emit('out', table, modeString(mode));
 }
 
-export function onDrag(dragger, table, mode,) {
-  dragger.dragging = true
-  dragger.emit('drag', table, mode);
-}
-
-function modeString(mode) {
-  return mode === columnType ? 'column' : 'row'
-}
+function emitter(thing = {}) {
+  const evt = {};
+  thing.on = (type, fn) => {
+    evt[type] = evt[type] || [];
+    evt[type].push(fn);
+    return thing;
+  };
+  thing.emit = (type, ...args) => {
+    if (!evt[type]) {
+      return;
+    }
+    for (const fn of evt[type]) {
+      fn(...args);
+    }
+  };
+  return thing;
 }
 
 export default function tableDragger(table, userOptions) {
@@ -102,216 +200,53 @@ export default function tableDragger(table, userOptions) {
   const dragger = emitter({
     dragging: false,
   });
-
+  // Catch the moment user drag the table
   const firstDrag$ = down$.pipe(
     filter(isMousedownValid),
     mergeMap((downEvent) => {
       return move$.pipe(
         takeUntil(up$),
         map(R.partial(realMode, [downEvent])),
-        firstDrag$.subscribe(({targetIndex, mode}) => {
-          const onlyBody = options.onlyBody && optionMode === 'row'
-          addClass(table, classes.originTable)
-          const fakeTable = R.compose(
-            R.curry(renderFakeTable)(table),
-            getWholeFakeTable)(table, mode)
+        filter(R.identity),
+        first(),
+        map(mode => ({
+          targetIndex: getTargetIndexInTable(downEvent.target, mode),
+          mode
+        }))
+      )
+    }),
+  )
+  firstDrag$.subscribe(({targetIndex, mode}) => {
+    const onlyBody = options.onlyBody && optionMode === 'row'
 
-          if (onlyBody) {
-            R.forEach(function ([fakeRow, realRow]) {
-              if (getOrganByCell(realRow).nodeName !== 'TBODY') {
-                addClass(fakeRow, classes.static)
-              }
-            })(R.zip([...fakeTable.children], [...table.rows]))
-          }
+    addClass(table, classes.originTable)
+    const fakeTable = R.compose(
+      R.curry(renderFakeTable)(table),
+      getWholeFakeTable)(table, mode)
 
-          export function exchangeColumns(table, from, to) {
+    if (onlyBody) {
+      R.forEach(function ([fakeRow, realRow]) {
+        if (getOrganByCell(realRow).nodeName !== 'TBODY') {
+          addClass(fakeRow, classes.static)
+        }
+      })(R.zip([...fakeTable.children], [...table.rows]))
+    }
 
-          }
-
-          export function exchangeColumns(table, from, to) {
-            if (from === to) {
-              return;
-            }
-            Array.from(table.rows).forEach((row) => {
-              sortElements(row.children[from], row.children[to], from < to)
-            });
-
-            const cols = table.querySelectorAll('col');
-            if (cols.length) {
-              sortElements(cols[from], cols[to], from < to);
-            }
-          }
-
-          export function exchangeRows(table, from, to) {
-            if (from === to) {
-              return;
-            }
-            const list = Array.from(table.rows);
-            sortElements(list[from], list[to], from < to)
-          }
-
-          export function onDrag(dragger, table, mode,) {
-            dragger.dragging = true
-            dragger.emit('drag', table, mode);
-          }
-
-
-          function isMousedownValid(mousedownEvent) {
-            const ignore = !isLeftButton(mousedownEvent) || mousedownEvent.metaKey || mousedownEvent.ctrlKey;
-            return !ignore
-          }
-
-          export function getRealMode(optionMode, moveDirection) {
-            if (!moveDirection) {
-              return
-            }
-            return optionMode === 'free' ? moveDirection : optionMode === 'row' ? rowType : columnType
-          }
-
-
-          export function getTableLength(table, mode) {
-            const getRowLength = R.compose(
-              R.partialRight(prop, ['length']),
-              R.partialRight(prop, ['children'])
-            )
-            return mode === rowType ?
-              table.rows.length :
-              R.compose(R.apply(Math.max), R.map(getRowLength), ArrayFrom)(table.rows)
-          }
-
-          export function getFakeTableByIndex(table, mode, index) {
-            const getFakeTable = mode === rowType ? getRowFakeTableByIndex : getColumnFakeTableByIndex
-            const attrsToRemove = ['width', 'height', 'id']
-            return R.pipe(
-              getFakeTable,
-              R.partialRight(removeAttrs, [attrsToRemove]),
-              R.partialRight(removeClass, [classes.originTable]),
-              removeAllCols,
-            )(table, index)
-          }
-
-          export function getTableLength(table, mode) {
-            R.partialRight(prop, ['length']),
-              R.partialRight(prop, ['children'])
-          }
-
-          export function getColumnFakeTableByIndex(table, index) {
-            const cells = R.map(R.partial(getCellByIndexInRow, [index]))(ArrayFrom(table.rows))
-            const fakeTable = R.pipe(cloneNode(false),
-              // set table height
-              R.partialRight(setStyle, ['height', addPx(table.clientHeight),]),
-              // set table width
-              R.partialRight(setStyle, ['width', addPx(cells[0].clientWidth),])
-            )(table)
-            return R.reduce(function (fakeTable, cell) {
-              // const realOrgan = getOrganByCell(cell)
-              return R.pipe(
-                R.partialRight(setStyle, ['height', addPx(cell.clientHeight)]),
-                // (realOrgan && !fakeTable.querySelector(realOrgan.nodeName)) ? R.partial(appendDOMChild, [cloneNode(false)(realOrgan)]) : R.identity,
-                R.curry(appendDOMChild)(fakeTable))
-              (cell)
-            })
-            (fakeTable)
-            (cells)
-          }
-
-
-          export function getColumnCellsByIndex(table, index) {
-            return R.compose(
-              R.map((row) => {
-                return row.children[index]
-              }),
-              ArrayFrom)(table.rows)
-          }
-
-          function modeString(mode) {
-            return mode === columnType ? 'column' : 'row'
-          }
-
-          export function getRowFakeTableByIndex(table, index) {
-            const realRow = table.rows[index]
-            const realOrgan = getOrganByCell(realRow)
-            const fakeTable = R.pipe(
-              cloneNode(true),
-              realOrgan ? R.curry(appendDOMChild)(clone(false)(real)) : R.identity,
-              R.curry(appendDOMChild)(cloneNode(false)(table))
-            )(fakeF)
-            const tuple = R.zip(
-              ArrayFrom(fakeTable.rows[0].children)
-            )
-            R.forEach(function ([realCell, fakeCell]) {
-              setStyle(fakeCell, 'height', addPx(prop(realCell, 'clientWidth')))
-            })(tuple)
-          }
-
-          export function getTableLength(table, mode) {
-            const getRowLength = R.compose(
-            )
-            return mode === rowType ?
-              table.rows.length :
-              R.compose(R.apply(Math.max), R.map(getRowLength), ArrayFrom)(table.rows)
-          }
-
-          export function getFakeTables(table, mode) {
-            const tableLength = getTableLength(table, mode)
-            return R.map(R.partial(getFakeTableByIndex, [table, mode]))(createArrByNumber(tableLength))
-          }
-
-          export function getColumnCellsByIndex(table, index) {
-            return R.compose(
-              R.map((row) => {
-                return row.children[index]
-              }),
-              ArrayFrom)(table.rows)
-          }
-
-          dragula([fakeTable], {
-            animation: 300,
-            staticClass: classes.static,
-            direction: mode === columnType ? 'horizontal' : 'vertical',
-          })
-            .on('drag', () => {
-              return onDrag(dragger, table, mode)
-            })
-            .on('dragend', R.partial(onDrop, [targetIndex, fakeTable, mode, table, dragger]))
-            .on('shadow', R.partial(onShadow, [targetIndex, fakeTable, mode, table, dragger]))
-            .on('out', () => {
-              return onOut(mode, table, dragger)
-            });
-
-
-          function emitter(thing = {}) {
-            const evt = {};
-            thing.on = (type, fn) => {
-              evt[type] = evt[type] || [];
-              evt[type].push(fn);
-              return thing;
-            };
-            thing.emit = (type, ...args) => {
-              if (!evt[type]) {
-                return;
-              }
-              for (const fn of evt[type]) {
-                fn(...args);
-              }
-            };
-            return thing;
-          }
-
-
-
-          export function getTargetIndexInTable(target, mode) {
-            while (target.nodeName !== 'TD' && target.nodeName !== 'TH') {
-              target = target.parentElement;
-            }
-            return mode === rowType ? target.parentElement.rowIndex : target.cellIndex
-          }
-
-
-          export function getMoveDirection(downEvent, moveEvent) {
-            const gapX = Math.abs(moveEvent.clientX - downEvent.clientX)
-            const gapY = Math.abs(moveEvent.clientY - downEvent.clientY)
-            if (gapX === gapY) return null
-            return gapX > gapY ? columnType : rowType
-          }
+    dragula([fakeTable], {
+      animation: 300,
+      staticClass: classes.static,
+      direction: mode === columnType ? 'horizontal' : 'vertical',
+    })
+      .on('drag', () => {
+        return onDrag(dragger, table, mode)
+      })
+      .on('dragend', R.partial(onDrop, [targetIndex, fakeTable, mode, table, dragger]))
+      .on('shadow', R.partial(onShadow, [targetIndex, fakeTable, mode, table, dragger]))
+      .on('out', () => {
+        return onOut(mode, table, dragger)
+      });
+    fakeTable.children[targetIndex].dispatchEvent(getMouseDownEvent());
+  })
+  return dragger
+}
 
