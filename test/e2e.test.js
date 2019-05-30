1ae808174c5285eb9b3892aeaf58c0644e29b515
lijun30 <iamdavidli@outlook.com>
2019-05-30 09:24:32 +0800

    testcafe

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -12,8 +12,11 @@ const watchState = ClientFunction(() => {
     const table = document.querySelector(`#${tableName}`)
     if (fakeTable) {
       window.dragginInfo = {
-        tableWidth: window.getComputedStyle(table).width,
+        hasFakeTable: !!fakeTable,
         tableHide: window.getComputedStyle(table).visibility === 'hidden',
+        tableHasOriginClass: table.classList.contains(classes.originTable),
+        hasDragginClass: fakeTable.parentElement.classList.contains(classes.dragging),
+        fakeTableBox: JSON.parse(JSON.stringify(fakeTable.getBoundingClientRect())),
         tableBox: JSON.parse(JSON.stringify(table.getBoundingClientRect()))
       }
 
@@ -21,18 +24,11 @@ const watchState = ClientFunction(() => {
     }
   };
 
-  await t.expect(draggingInfo).ok()
-    .expect(draggingInfo.hasFakeTable).eql(true)
-    .expect(draggingInfo.tableHide).eql(true)
-    .expect(draggingInfo.tableHasOriginClass).eql(true)
-    .expect(draggingInfo.hasDragginClass).eql(true)
-    .expect(draggingInfo.fakeTableBox).eql(draggingInfo.tableBox)
-
-
   window.addEventListener('mousemove', mouseMoveHandler);
 });
-const watchState = ClientFunction(() => {
-  const mouseMoveHandler = () => {
+
+function testTable({name, startNum, endNum, mode, desc}) {
+  test(desc, async t => {
     const tableName = name
     const table = await Selector(`#${tableName}`)
     const start = table.find(`.${mode === 'column' ? 'c' : 'r'}${startNum}`)
@@ -44,27 +40,42 @@ const watchState = ClientFunction(() => {
         tableName
       }
     })();
-    const classes = JSON.parse(classesStr)
-    const fakeTable = document.querySelector(`.${classes.fakeTable}`)
-    const table = document.querySelector(`#${tableName}`)
-    if (fakeTable) {
-      window.dragginInfo = {
-        tableWidth: window.getComputedStyle(table).width,
-        tableHide: window.getComputedStyle(table).visibility === 'hidden',
-        tableBox: JSON.parse(JSON.stringify(table.getBoundingClientRect()))
+    await t.dragToElement(start, end, {
+      destinationOffsetX: 50,
+      destinationOffsetY: 50
+    });
+    const draggingInfo = await t.eval(() => window.dragginInfo)
+    await t.expect(draggingInfo).ok()
+      .expect(draggingInfo.hasFakeTable).eql(true)
+      .expect(draggingInfo.tableHide).eql(true)
+      .expect(draggingInfo.tableHasOriginClass).eql(true)
+      .expect(draggingInfo.hasDragginClass).eql(true)
+      .expect(draggingInfo.fakeTableBox).eql(draggingInfo.tableBox)
+
+    if (mode === 'column') {
+      if (startNum < endNum) {
+        // 所以要获得props要await并执行，就像上面的table，await并执行了，find到的子元素就不用再来一遍了
+        // 所以最后一定要带一个括号，不然就不是执行了
+        // start.innerText这种打印是打不出来的，只会等到assert的时候再计算
+        const next = await end.nextSibling(0)()
+        // await t.expect(start.innerText).eql(next.innerText)
+      } else {
+        // -1才是最近的！1是最远的，这个api排序和文档说的不一样
+        const previous = await end.prevSibling(-1)()
+        await t.expect(start.innerText).eql(previous.innerText)
       }
-
-      window.removeEventListener('mousemove', mouseMoveHandler);
     }
-  };
-
-  await t.expect(draggingInfo).ok()
-    .expect(draggingInfo.hasFakeTable).eql(true)
-    .expect(draggingInfo.tableHide).eql(true)
-    .expect(draggingInfo.tableHasOriginClass).eql(true)
-    .expect(draggingInfo.hasDragginClass).eql(true)
-    .expect(draggingInfo.fakeTableBox).eql(draggingInfo.tableBox)
-
-
-  window.addEventListener('mousemove', mouseMoveHandler);
-});
+    if (mode === 'row') {
+      const startRow = await start.parent(0)()
+      if (startNum < endNum) {
+        // 这样写是不行的，要下面那样
+        // const next = await endRow.nextSibling(0)()
+        const next = await end.parent(0).nextSibling(0)()
+        await t.expect(startRow.innerText).eql(next.innerText)
+      } else {
+        const previous = await end.parent(0).prevSibling(0)()
+        await t.expect(startRow.innerText).eql(previous.innerText)
+      }
+    }
+  });
+}
